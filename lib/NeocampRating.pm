65666d92e7f678749e7d04b9d8cce4bb34fe0427
Pavel Tolstov <ray66rus@gmail.com>
2014-05-05 19:35:04 +0400

    added admin interface

--- lib/NeocampRating.pm
+++ lib/NeocampRating.pm
@@ -6,7 +6,7 @@ use NCRUser;
 use Schema;
 
 has schema => sub {
-	return Schema->connect('dbi:SQLite:' . ($ENV{DB_FILE_NAME} || "$FindBin::Bin/../db/main.db"));
+	return Schema->connect('dbi:SQLite:' . ($ENV{DB_FILE_PATH} || "$FindBin::Bin/../db") . "/main.db");
 };
 
 sub startup {
@@ -30,6 +30,12 @@ sub startup {
 	$logged_in->post('/save_ratings')->to('students#save_ratings');
 	$logged_in->get('/load_csv')->to('admin#load_csv_form');
 	$logged_in->post('/load_csv')->to('admin#parse_and_save_csv');
+	my $student_action = $logged_in->any('/student')->to(controller => 'students');
+	$student_action->get('/edit/:id')->to(action => 'edit');
+	$student_action->get('/delete/:id')->to(action => 'del');
+	$student_action->get('/add')->to(action => 'add');
+	$student_action->post('/add')->to(action => 'save');
+	$student_action->post('/edit/:id')->to(action => 'save');
 	$r->get('/logout')->to('login#logout');
 }
 
--- lib/NeocampRating/Students.pm
+++ lib/NeocampRating/Students.pm
@@ -1,6 +1,8 @@
 package NeocampRating::Students;
 use Mojo::Base 'Mojolicious::Controller';
 
+use FindBin;
+
 use constant PAGE_SIZE => 3;
 
 sub paged {
@@ -99,6 +101,124 @@ sub save_ratings {
 		$self->flash(message => 'Ratings saved', message_type => 'success');
 	}
 	return $self->redirect_to($self->req->headers->referrer);
+}
+
+sub del {
+	my $self = shift;
+
+	my $id = $self->stash('id');
+
+	my $student_rec = $self->db->resultset('Student')->search({id => $id});
+	my $student = $student_rec->single;
+	if(!$student) {
+		$self->flash(message => "Can't find student with id $id", message_type => 'error');
+		return $self->redirect_to($self->req->headers->referrer);
+	}
+
+	$self->flash(message => 'Student "' . $student->name . "' (" . $student->pid . ") deleted", message_type => 'success');
+	$student_rec->delete_all;
+
+	return $self->redirect_to($self->req->headers->referrer);
+}
+
+sub add {
+	my $self = shift;
+	return $self->render('students/edit_form', student => $self->db->resultset('Student')->new_result({id => ''}), referer => $self->req->headers->referrer);
+}
+
+sub edit {
+	my $self = shift;
+	my $options = shift;
+
+	my $id = $self->stash('id');
+
+	my $student_rec = $self->db->resultset('Student')->search({id => $id});
+	my $student = $student_rec->single;
+	if(!$student) {
+		$self->flash(message => "Can't find student with id $id", message_type => 'error');
+		return $self->redirect_to($self->req->headers->referrer);
+	}
+
+	return $self->render('students/edit_form', student => $student, referer => $self->req->headers->referrer);
+}
+
+sub save {
+	my $self = shift;
+
+	my %updated_vals = (
+		id => $self->param('id'),
+		pid => $self->param('pid'),
+		name => $self->param('name'),
+		rating => $self->param('rating')
+	);
+	$updated_vals{img} = $self->param('img_file')->filename
+		if $self->param('img_file')->filename;
+
+	my $is_new_user = ($updated_vals{id} eq '') ? 1 : 0;
+
+	if($updated_vals{pid} eq '') {
+		$self->stash(message => "ID field can't be empty", message_type => 'error');
+		my $student = $self->db->resultset('Student')->new_result(\%updated_vals);
+		return $self->render('students/edit_form', student => $student, referer => $self->param('referer'));
+	}
+
+	my $student = $self->db->resultset('Student')->search({pid => $updated_vals{pid}})->single;
+	if($student and ($updated_vals{id} eq '' or $student->id ne $updated_vals{id})) {
+		$self->stash(message => "User with personal id $updated_vals{pid} already exists", message_type => 'error');
+		$student = $self->db->resultset('Student')->new_result(\%updated_vals);
+		return $self->render('students/edit_form', student => $student, referer => $self->param('referer'));
+	}
+
+	if(!$self->_load_avatar) {
+		$student = $self->db->resultset('Student')->new_result(\%updated_vals);
+		return $self->render('students/edit_form', student => $student, referer => $self->param('referer'));
+	}
+
+	if($is_new_user) {
+		delete $updated_vals{id};
+		$self->db->resultset('Student')->create(\%updated_vals);
+		$self->flash(message => qq|User "$updated_vals{name}" ($updated_vals{pid}) added|, message_type => 'success');
+	} else {
+		my $student_rec = $self->db->resultset('Student')->search({id => $updated_vals{id}});
+		my $student = $student_rec->single; 
+		if(!$student) {
+			$self->flash(message => "Can't find student with id $updated_vals{id}", message_type => 'error');
+			return $self->redirect_to($self->param('referer'));
+		}
+		if($updated_vals{img}) {
+			unlink(($ENV{DB_FILE_PATH} || $FinBin::Bin) . '/../public/img/avatars/' . $student->img);
+		} else {
+			delete $updated_vals{img};
+		}
+		$student_rec->update(\%updated_vals);
+		$self->flash(message => qq|User "$updated_vals{name}" ($updated_vals{id}) updated|, message_type => 'success');
+	}
+
+	return $self->redirect_to($self->param('referer'));
+}
+
+sub _load_avatar {
+	my $self = shift;
+	my $filedata = $self->req->upload('img_file');
+
+	if(!$filedata) {
+		$self->stash(message => 'File is not available', message_type => 'error');
+		return 0;
+	}
+
+	if($self->req->is_limit_exceeded) {
+		$self->stash(message => 'File is too big', message_type => 'error');
+		return 0;
+	}
+
+	if($filedata->filename eq '') {
+		$self->stash(message => 'Filename must not be empty', message_type => 'error');
+		return 0;
+	}
+
+	$filedata->move_to(($ENV{DB_FILE_PATH} || $FinBin::Bin) . '/../public/img/avatars/' . $filedata->filename);
+
+	return 1;
 }
 
 1;
